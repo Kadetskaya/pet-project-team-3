313d89eab644259bf4785e25b13952c7e9b16ab5
AlexeyLocker <alexey.locker@gmail.com>
2020-03-22 21:41:25 +0200

    Js Twitter Adds

---
+++ Twitter/src/js/main.js
@@ -0,0 +1,14 @@
+"use strict"
+let head_click = document.querySelectorAll('.inner-head');
+
+head_click.forEach(element => {
+    element.addEventListener("click", (e) => {
+        head_click.forEach(element => {
+            if (element.classList.contains("nav-content_active")) {
+                element.classList.remove("nav-content_active");
+            }
+
+        })
+        e.target.classList.add("nav-content_active");
+    })
+})
